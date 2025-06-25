572f6802ac30bea1110e8e664b68df9b4ba378cb
RC Maples <rc@rcmaples.io>
2025-06-25 07:52:43 -0400

    cleaning eslint config

--- apps/web/eslint.config.mjs
+++ apps/web/eslint.config.mjs
@@ -2,20 +2,3 @@ import {baseConfig} from '@workspace/eslint-config/base'
 import {nextConfig} from '@workspace/eslint-config/next'
 
 export default [...baseConfig, ...nextConfig]
-
-// import { dirname } from 'path';
-// import { fileURLToPath } from 'url';
-// import { FlatCompat } from '@eslint/eslintrc';
-
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = dirname(__filename);
-
-// const compat = new FlatCompat({
-//   baseDirectory: __dirname,
-// });
-
-// const eslintConfig = [
-//   ...compat.extends('next/core-web-vitals', 'next/typescript'),
-// ];
-
-// export default eslintConfig;
